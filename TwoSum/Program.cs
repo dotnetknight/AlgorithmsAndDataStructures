04371b696ab9089f5bfe95e98e6019ce17a3646c
Niko Kantaria <nika.qantaria98@gmail.com>
2022-09-14 17:33:40 +0400

    TwoSum

---
+++ TwoSum/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace TwoNum
+{
+    internal class Program
+    {
+        public static int[] TwoNumberSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                int firstNum = nums[i];
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    int secondNum = nums[j];
+                    if (firstNum + secondNum == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return Array.Empty<int>();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter numbers:");
+            int[]? nums = Array.ConvertAll(Console.ReadLine().Trim().Split(','), Convert.ToInt32);
+
+            Console.WriteLine("Enter target:");
+            int target = Convert.ToInt32(Console.ReadLine());
+
+            var result = TwoNumberSum(nums, target);
+
+            foreach (var r in result)
+            {
+                Console.Write(r.ToString() + " ");
+            }
+        }
+    }
+}
