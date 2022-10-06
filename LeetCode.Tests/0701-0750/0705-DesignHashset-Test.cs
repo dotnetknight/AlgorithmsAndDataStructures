80ca4693602f2b3bca334053c47269b3df27520f
Niko Kantaria <niko.kantaria@dataart.com>
2022-10-06 07:22:29 +0400

    0705. DesignHashset

---
+++ LeetCode.Tests/0701-0750/0705-DesignHashset-Test.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode._0701_0750;
+
+namespace LeetCode.Tests._0701_0750
+{
+    public class _0705_DesignHashset_Test
+    {
+        [Test]
+        public void DesignHashSet()
+        {
+            var hashSet = new _0705_DesignHashset();
+
+            hashSet.Add(1);
+            hashSet.Add(2);
+            Assert.IsTrue(hashSet.Contains(1));
+            Assert.IsFalse(hashSet.Contains(3));
+            hashSet.Add(2);
+            Assert.IsTrue(hashSet.Contains(2));
+            hashSet.Remove(2);
+            Assert.IsFalse(hashSet.Contains(2));
+        }
+    }
+}
---
+++ LeetCode/0701-0750/0705-DesignHashset.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._0701_0750
+{
+    public class _0705_DesignHashset
+    {
+        private bool[] arr = new bool[1000001];
+
+        public _0705_DesignHashset()
+        {
+        }
+
+        public void Add(int key)
+        {
+            arr[key] = true;
+        }
+
+        public void Remove(int key)
+        {
+            if (Contains(key)) arr[key] = false;
+        }
+
+        public bool Contains(int key)
+        {
+            return arr[key];
+        }
+    }
+}
