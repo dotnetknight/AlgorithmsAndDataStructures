89494af67773ef30935146a4c07ab07565c8c09a
Niko Kantaria <niko.kantaria@dataart.com>
2022-10-10 15:50:21 +0400

    771. Jewels and Stones

---
+++ LeetCode.Tests/0751-0800/0771-JewelsAndStones-Test.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode._0751_0800;
+
+namespace LeetCode.Tests._0751_0800
+{
+    public class _0771_JewelsAndStones_Test
+    {
+        [Test]
+        [TestCase("aA", "aAAbbbb", 3)]
+        [TestCase("z", "ZZ", 0)]
+        [TestCase("abcd", "ABabCPOddcKa", 6)]
+        public void NumJewelsInStonesTest(string jewels, string stones, int stonesThatAreAlsoJewels)
+        {
+            var solution = new _0771_JewelsAndStones();
+            var result = solution.NumJewelsInStones(jewels, stones);
+
+            Assert.That(result, Is.EqualTo(stonesThatAreAlsoJewels));
+        }
+    }
+}
---
+++ LeetCode/0751-0800/0771-JewelsAndStones.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode._0751_0800
+{
+    public class _0771_JewelsAndStones
+    {
+        public int NumJewelsInStones(string jewels, string stones)
+        {
+            var hashMap = new Dictionary<char, int>();
+            int stonesThatAreAlsoJewels = 0;
+
+            foreach (var character in jewels)
+            {
+                hashMap.Add(character, 0);
+            }
+
+            foreach (var character in stones)
+            {
+                if (hashMap.ContainsKey(character))
+                {
+                    stonesThatAreAlsoJewels++;
+                }
+            }
+            return stonesThatAreAlsoJewels;
+        }
+    }
+}
