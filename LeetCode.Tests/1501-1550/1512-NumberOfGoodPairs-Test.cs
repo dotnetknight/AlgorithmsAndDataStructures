5a81dae19772288b668cf09e17a78bc4d3858a6b
Niko Kantaria <niko.kantaria@dataart.com>
2022-10-10 15:33:17 +0400

    1512. Number of Good Pairs

---
+++ LeetCode.Tests/1501-1550/1512-NumberOfGoodPairs-Test.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode._1501_1550;
+
+namespace LeetCode.Tests._1501_1550
+{
+    public class _1512_NumberOfGoodPairs_Test
+    {
+        [Test]
+        [TestCase(new int[] { 1, 2, 3, 1, 1, 3 }, 4)]
+        [TestCase(new int[] { 1, 1, 1, 1 }, 6)]
+        [TestCase(new int[] { 1, 2, 3 }, 0)]
+        public void NumberOfGoodPairs_Test(int[] nums, int expected)
+        {
+            var solution = new _1512_NumberOfGoodPairs();
+            var result = solution.NumIdenticalPairs(nums);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ LeetCode/1501-1550/1512-NumberOfGoodPairs.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode._1501_1550
+{
+    public class _1512_NumberOfGoodPairs
+    {
+        public int NumIdenticalPairs(int[] nums)
+        {
+            int numberOfGoodPairs = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j] && i < j)
+                    {
+                        numberOfGoodPairs++;
+                    }
+                }
+            }
+
+            return numberOfGoodPairs;
+        }
+    }
+}
