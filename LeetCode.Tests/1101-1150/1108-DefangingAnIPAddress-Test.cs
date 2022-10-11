f77796c2322dc1b984af6074655f1fb0f58ae9fd
Niko Kantaria <niko.kantaria@dataart.com>
2022-10-11 20:17:04 +0400

    1108. Defanging an IP Address

---
+++ LeetCode.Tests/1101-1150/1108-DefangingAnIPAddress-Test.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode._1101_1150;
+
+namespace LeetCode.Tests._1101_1150
+{
+    public class _1108_DefangingAnIPAddress_Test
+    {
+        [Test]
+        [TestCase("1.1.1.1", "1[.]1[.]1[.]1")]
+        [TestCase("255.100.50.0", "255[.]100[.]50[.]0")]
+        public void DefangingAnIPAddress(string ip, string expectedResult)
+        {
+            var solution = new _1108_DefangingAnIPAddress();
+            var result = solution.DefangIPaddr(ip);
+
+            Assert.That(expectedResult, Is.EqualTo(result));
+        }
+    }
+}
---
+++ LeetCode/1101-1150/1108-DefangingAnIPAddress.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace LeetCode._1101_1150
+{
+    public class _1108_DefangingAnIPAddress
+    {
+        public string DefangIPaddr(string address)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < address.Length; i++)
+            {
+                stringBuilder.Append(address[i] != '.' ? address[i] : "[.]");
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
