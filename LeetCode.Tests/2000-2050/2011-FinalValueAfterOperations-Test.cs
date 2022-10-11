c28fcbba1e82103a5a9e3db2e4cbf98ea0cfc875
Niko Kantaria <niko.kantaria@dataart.com>
2022-10-11 20:35:48 +0400

    2011. Final Value of Variable After Performing Operations

---
+++ LeetCode.Tests/2000-2050/2011-FinalValueAfterOperations-Test.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode._2000_2050;
+
+namespace LeetCode.Tests._2000_2050
+{
+    public class _2011_FinalValueAfterOperations_Test
+    {
+        [Test]
+        [TestCase(new string[] { "--X", "X++", "X++" }, 1)]
+        [TestCase(new string[] { "++X", "++X", "X++" }, 3)]
+        [TestCase(new string[] { "X++", "++X", "--X", "X--" }, 0)]
+        public void FinalValueAfterOperations(string[] operations, int valueAfterOperations)
+        {
+            var solution = new _2011_FinalValueAfterOperations();
+            var result = solution.FinalValueAfterOperations(operations);
+
+            Assert.That(valueAfterOperations, Is.EqualTo(result));
+        }
+    }
+}
---
+++ LeetCode/2000-2050/2011-FinalValueAfterOperations.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode._2000_2050
+{
+    public class _2011_FinalValueAfterOperations
+    {
+        public int FinalValueAfterOperations(string[] operations)
+        {
+            int X = 0;
+
+            foreach (string operation in operations)
+            {
+                if (operation.Contains('+'))
+                {
+                    X++;
+                }
+                else
+                {
+                    X--;
+                }
+            }
+
+            return X;
+        }
+    }
+}
