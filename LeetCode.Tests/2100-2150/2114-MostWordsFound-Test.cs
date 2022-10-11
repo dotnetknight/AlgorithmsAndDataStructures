e4855c69437a555bc6fca61dbc40f5dc949249f7
Niko Kantaria <niko.kantaria@dataart.com>
2022-10-11 20:56:44 +0400

    2114. Maximum Number of Words Found in Sentences

---
+++ LeetCode.Tests/2100-2150/2114-MostWordsFound-Test.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode._2100_2150;
+
+namespace LeetCode.Tests._2100_2150
+{
+    public class _2114_MostWordsFound_Test
+    {
+        [Test]
+        [TestCase(new string[] { "alice and bob love leetcode", "i think so too", "this is great thanks very much" }, 6)]
+        [TestCase(new string[] { "please wait", "continue to fight", "continue to win" }, 3)]
+        [TestCase(new string[] { "whatever it takes", "deshi basara", "supreme" }, 3)]
+        public void MostWordsFound(string[] sentences, int maximumNumberOfWords)
+        {
+            var solution = new _2114_MostWordsFound();
+            var result = solution.MostWordsFound(sentences);
+            Assert.That(result, Is.EqualTo(maximumNumberOfWords));
+        }
+    }
+}
---
+++ LeetCode/2100-2150/2114-MostWordsFound.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode._2100_2150
+{
+    public class _2114_MostWordsFound
+    {
+        public int MostWordsFound(string[] sentences)
+        {
+            int max = 0;
+
+            foreach (string sentence in sentences)
+            {
+                int numberOfWords = sentence.Split(" ").Length;
+                if (numberOfWords > max)
+                {
+                    max = numberOfWords;
+                }
+            }
+
+            return max;
+        }
+    }
+}
