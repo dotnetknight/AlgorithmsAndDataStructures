56c3b58ec0ff81d7045dbb1ea231388a70d0c110
Niko Kantaria <nika.qantaria98@gmail.com>
2022-10-18 19:04:58 +0400

    1791. Find Center of Star Graph

---
+++ LeetCode.Tests/1750-1800/1791-FindCenterofStarGraph-Test.cs
@@ -0,0 +1,17 @@
+﻿using LeetCode._1750_1800;
+
+namespace LeetCode.Tests._1750_1800
+{
+    public class _1791_FindCenterofStarGraph_Test
+    {
+        [Test]
+        public void FindCenterofStarGraph()
+        {
+            var edges = new int[][] { new int[] { 1, 2 }, new int[] { 2, 3 }, new int[] { 4, 2 } };
+
+            var solution = new _1791_FindCenterofStarGraph();
+            var result = solution.FindCenter(edges);
+            Assert.That(result, Is.EqualTo(2));
+        }
+    }
+}
---
+++ LeetCode/1750-1800/1791-FindCenterofStarGraph.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode._1750_1800
+{
+    public class _1791_FindCenterofStarGraph
+    {
+        public int FindCenter(int[][] edges)
+        {
+            var oneDimentionalArrayLenght = edges.Length / 2;
+
+            List<int> zeroIndexedElements = new();
+            zeroIndexedElements.Add(edges[0][0]);
+            bool containsValue = false;
+
+            for (int i = 1; i < oneDimentionalArrayLenght; i++)
+            {
+                if (zeroIndexedElements.Contains(edges[i][0]))
+                {
+                    return edges[i][0];
+                }
+                else
+                {
+                    zeroIndexedElements.Add(edges[i][0]);
+                }
+            }
+
+            if (!containsValue)
+            {
+                if (edges[0][1] == edges[1][1])
+                {
+                    return edges[0][1];
+                }
+                else if (edges[0][1] == edges[2][1])
+                {
+                    return edges[0][1];
+                }
+                else
+                {
+                    return edges[1][1];
+                }
+            }
+
+            return 0;
+        }
+    }
+}
