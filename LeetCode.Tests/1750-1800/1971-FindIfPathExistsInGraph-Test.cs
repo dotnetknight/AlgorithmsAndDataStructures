977546e1d01065556858ab43b81c52962ed46c4f
Niko Kantaria <nika.qantaria98@gmail.com>
2022-11-11 21:01:14 +0400

    FindIfPathExistsInGraph

---
+++ LeetCode.Tests/1750-1800/1971-FindIfPathExistsInGraph-Test.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode._1750_1800;
+
+namespace LeetCode.Tests._1750_1800
+{
+    public class _1971_FindIfPathExistsInGraph_Test
+    {
+        [Test]
+        public void FindIfPathExistsInGraph()
+        {
+            var edges = new int[][] { new int[] { 0, 1 }, new int[] { 1, 2 }, new int[] { 2, 0 } };
+            int n = 3, source = 0, destination = 2;
+
+            var solution = new _1971_FindIfPathExistsInGraph();
+            var result = solution.ValidPath(n, edges, source, destination);
+            Assert.That(result, Is.True);
+        }
+    }
+}
---
+++ LeetCode/1750-1800/1971-FindIfPathExistsInGraph.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode._1750_1800
+{
+    public class _1971_FindIfPathExistsInGraph
+    {
+        public bool ValidPath(int n, int[][] edges, int source, int destination)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (edges[i][source] == destination || edges[source][i] == destination)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
