f5679c72e30a5fc8c0c037742e6cb9c99b5f52d9
Niko Kantaria <nika.qantaria98@gmail.com>
2022-11-12 23:27:43 +0400

    SelectionSort

---
+++ Algorithms.Test/SelectionSort-Test.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+
+namespace Algorithms.Test
+{
+    public class SelectionSortTestCaseData : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new int[] { 2, 22 };
+            yield return new int[] { 1, 2, 3 };
+            yield return new int[] { 3, 12, 23, 5, 0, 50 };
+            yield return new int[] { -2, 4, 55, 23, 3, 1, 40 };
+        }
+    }
+
+    public class SelectionSort_Test
+    {
+        [Test]
+        [TestCaseSource(typeof(SelectionSortTestCaseData))]
+        public void SelectionSort_HasArrayToPerformSelectionSort_ReturnsSortedArray(int[] array)
+        {
+            var solution = new SelectionSort();
+            var sortedArray = solution.Sort(array);
+            Array.Sort(array);
+
+            CollectionAssert.AreEquivalent(sortedArray, array);
+        }
+
+        [Test]
+        public void SelectionSort_HasEmptyArray_ReturnsEmptyArray()
+        {
+            var solution = new SelectionSort();
+            var array = Array.Empty<int>();
+
+            var sortedArray = solution.Sort(array);
+            Assert.That(sortedArray, Is.Empty);
+        }
+    }
+}
---
+++ Algorithms/SelectionSort.cs
@@ -0,0 +1,42 @@
+﻿namespace Algorithms
+{
+    public class SelectionSort
+    {
+        // Best: O(n^2) time | O(1) space
+        // Average: O(n^2) time | O(1) space
+        // Worst: O(n^2) time | O(1) space
+        public int[] Sort(int[] array)
+        {
+            if (array.Length == 0)
+            {
+                return Array.Empty<int>();
+            }
+
+            int startIdx = 0;
+            while (startIdx < array.Length - 1)
+            {
+                int smallestIdx = startIdx;
+                
+                for (int i = startIdx + 1; i < array.Length; i++)
+                {
+                    if (array[smallestIdx] > array[i])
+                    {
+                        smallestIdx = i;
+                    }
+                }
+
+                Swap(startIdx, smallestIdx, array);
+                startIdx++;
+            }
+
+            return array;
+        }
+
+        public void Swap(int i, int j, int[] array)
+        {
+            int temp = array[j];
+            array[j] = array[i];
+            array[i] = temp;
+        }
+    }
+}
