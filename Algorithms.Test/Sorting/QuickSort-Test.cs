71aedab7391786c0283fb1452a915f5c074b7b5f
Niko Kantaria <nika.qantaria98@gmail.com>
2022-11-15 20:47:51 +0400

    QuickSort

---
+++ Algorithms.Test/Sorting/QuickSort-Test.cs
@@ -0,0 +1,18 @@
+﻿using Algorithms.Sorting;
+using Algorithms.Test.TestCaseData;
+
+namespace Algorithms.Test.Sorting
+{
+    public class QuickSort_Test
+    {
+        [Test]
+        [TestCaseSource(typeof(IntegerArrayTestCaseData))]
+        public void QuickSort_HasArrayToPerformQuickSort_SortsArray(int[] array)
+        {
+            var solution = new QuickSort();
+            var sortedArray = solution.Sort(array);
+
+            Assert.That(sortedArray, Is.Ordered);
+        }
+    }
+}
---
+++ Algorithms/Sorting/QuickSort.cs
@@ -0,0 +1,58 @@
+﻿namespace Algorithms.Sorting
+{
+    public class QuickSort
+    {
+        // Best: O(nlog(n)) time | O(log(n)) space
+        // Average: O(nlog(n)) time | O(log(n)) space
+        // Worst: O(n^2) time | O(log(n)) space
+        public int[] Sort(int[] array)
+        {
+            Sort(array, 0, array.Length - 1);
+            return array;
+        }
+
+        private void Sort(int[] array, int startIdx, int endIdx)
+        {
+            if (startIdx >= endIdx)
+            {
+                return;
+            }
+
+            int pivotIdx = startIdx;
+            int leftIdx = startIdx + 1;
+            int rightIdx = endIdx;
+
+            while (rightIdx >= leftIdx)
+            {
+                if (array[leftIdx] > array[pivotIdx] && array[rightIdx] < array[pivotIdx])
+                {
+                    BaseSwap.SwapIntegers(leftIdx, rightIdx, array);
+                }
+
+                if (array[leftIdx] <= array[pivotIdx])
+                {
+                    leftIdx += 1;
+                }
+
+                if (array[rightIdx] >= array[pivotIdx])
+                {
+                    rightIdx -= 1;
+                }
+            }
+
+            BaseSwap.SwapIntegers(pivotIdx, rightIdx, array);
+
+            bool leftSubarrayIsSmaller = rightIdx - 1 - startIdx < endIdx - (rightIdx + 1);
+            if (leftSubarrayIsSmaller)
+            {
+                Sort(array, startIdx, rightIdx - 1);
+                Sort(array, rightIdx + 1, endIdx);
+            }
+            else
+            {
+                Sort(array, rightIdx + 1, endIdx);
+                Sort(array, startIdx, rightIdx - 1);
+            }
+        }
+    }
+}
