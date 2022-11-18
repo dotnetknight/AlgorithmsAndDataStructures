2fbf1843ffba22153f8275cbdc6a3b2d7c8f0080
Niko Kantaria <nika.qantaria98@gmail.com>
2022-11-18 19:16:06 +0400

    344. Reverse String

---
+++ LeetCode.Tests/0301-0350/0344-ReverseString-Test.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode._0301_0350;
+using System.Collections;
+
+namespace LeetCode.Tests._0301_0350
+{
+    public class ReverseStringTestCaseData : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[] { new char[] { 'h', 'e', 'l', 'l', 'o' }, new char[] { 'o', 'l', 'l', 'e', 'h' } };
+            yield return new object[] { new char[] { 'b', 'a', 't', 'm', 'a', 'n' }, new char[] { 'n', 'a', 'm', 't', 'a', 'b' } };
+        }
+    }
+
+    public class _0344_ReverseString_Test
+    {
+        [Test]
+        [TestCaseSource(typeof(ReverseStringTestCaseData))]
+        public void ReverseString_HasStringToReverse_ReturnsReversedString(char[] chars, char[] reversedCharacters)
+        {
+            var solution = new _0344_ReverseString();
+            solution.ReverseString(chars);
+
+            CollectionAssert.AreEqual(chars, reversedCharacters);
+        }
+    }
+}
---
+++ LeetCode/0301-0350/0344_ReverseString.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode._0301_0350
+{
+    public class _0344_ReverseString
+    {
+        public void ReverseString(char[] s)
+        {
+            if (s == null || s.Length == 0)
+            {
+                return;
+            }
+
+            int i = 0, j = s.Length - 1;
+            while (i < j)
+            {
+                var temp = s[i];
+
+                s[i] = s[j];
+                s[j] = temp;
+
+                i++;
+                j--;
+            }
+        }
+    }
+}
