2df05dff33545ecdc0b6a38064881b62b8cd7645
Niko Kantaria <nika.qantaria98@gmail.com>
2022-11-18 19:36:23 +0400

    151. Reverse Words in a String

---
+++ LeetCode.Tests/0151-0200/0151-ReverseWordsInAString-Test.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode._0151_0200;
+using System.Collections;
+
+namespace LeetCode.Tests._0151_0200
+{
+    public class ReverseWordsInAStrTestCaseData : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[] { "The Dark Knight", "Knight Dark The" };
+            yield return new object[] { "the sky is blue", "blue is sky the" };
+            yield return new object[] { "  hello world!  ", "world! hello" };
+            yield return new object[] { "a good   developer", "developer good a" };
+        }
+    }
+
+    public class _0151_ReverseWordsInAString_Test
+    {
+        [Test]
+        [TestCaseSource(typeof(ReverseWordsInAStrTestCaseData))]
+        public void ReverseString_HasStringToReverse_ReturnsReversedString(string words, string reversed)
+        {
+            var solution = new _0151_ReverseWordsInAString();
+            var reversedString = solution.ReverseWords(words);
+
+            Assert.That(reversedString, Is.EqualTo(reversed));
+        }
+    }
+}
---
+++ LeetCode/0151-0200/0151-ReverseWordsInAString.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode._0151_0200
+{
+    public class _0151_ReverseWordsInAString
+    {
+        // O(n) time | O(n) space
+        public string ReverseWords(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                return string.Empty;
+            }
+
+            var stringToReverse = s.Trim().Split(' ');
+            var finalString = string.Empty;
+
+            for (int i = stringToReverse.Length - 1; i >= 0; i--)
+            {
+                if (stringToReverse[i] != "")
+                {
+                    finalString = string.Join(" ", finalString, stringToReverse[i]);
+                }
+            }
+
+            return finalString.Trim();
+        }
+    }
+}
