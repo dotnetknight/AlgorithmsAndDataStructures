8277ebfd2d44ffba15a1c163b59fbefc85e24357
Niko Kantaria <niko.kantaria@dataart.com>
2022-11-25 08:06:41 +0400

    5. Longest Palindromic Substring

---
+++ LeetCode.Tests/0001-0050/005_LongestPalindromicSubstring_Test.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode._0001_0050;
+
+namespace LeetCode.Tests._0001_0050
+{
+    public class _005_LongestPalindromicSubstring_Test
+    {
+        [Test]
+        [TestCase("abcdefgfedcba", "abcdefgfedcba")]
+        [TestCase("abcdefggfedcba", "abcdefggfedcba")]
+        [TestCase("ccc", "ccc")]
+        [TestCase("aaaaaaaaaa", "aaaaaaaaaa")]
+        [TestCase("", "")]
+        [TestCase("a", "a")]
+        [TestCase("aabccdccbaaeeggee", "aabccdccbaa")]
+        [TestCase("eegffgeeaabcdcbaa", "aabcdcbaa")]
+        [TestCase("abcdcbbcd", "dcbbcd")]
+        [TestCase("abcddcbebcd", "dcbebcd")]
+        public void LongestPalindrome_HasString_ReturnsExpectedResult(string str, string expectedString)
+        {
+            var solution = new _005_LongestPalindromicSubstring();
+            var result = solution.LongestPalindrome(str);
+
+            Assert.That(result, Is.EqualTo(expectedString));
+        }
+    }
+}
---
+++ LeetCode/0001-0050/005_LongestPalindromicSubstring.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode._0001_0050
+{
+    public class _005_LongestPalindromicSubstring
+    {
+        public string LongestPalindrome(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s) || s.Length == 1)
+            {
+                return s;
+            }
+
+            int[] currentLongest = { 0, 1 };
+            for (int i = 1; i < s.Length; i++)
+            {
+                int[] odd = GetLongestPalindromeFrom(s, i - 1, i + 1);
+                int[] even = GetLongestPalindromeFrom(s, i - 1, i);
+                int[] longest = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
+
+                currentLongest = currentLongest[1] - currentLongest[0] >
+                longest[1] - longest[0] ? currentLongest : longest;
+            }
+
+            return s.Substring(currentLongest[0], currentLongest[1] - currentLongest[0]);
+        }
+
+        private static int[] GetLongestPalindromeFrom(string str, int leftIdx, int rightIdx)
+        {
+            while (leftIdx >= 0 && rightIdx < str.Length)
+            {
+                if (str[leftIdx] != str[rightIdx])
+                {
+                    break;
+                }
+
+                leftIdx--;
+                rightIdx++;
+            }
+
+            return new int[] { leftIdx + 1, rightIdx };
+        }
+    }
+}
