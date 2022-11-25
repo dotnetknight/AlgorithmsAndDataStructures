fae3ada07019c63a74685658963f3448e9931713
Niko Kantaria <niko.kantaria@dataart.com>
2022-11-25 07:47:08 +0400

    3. Longest Substring Without Repeating Characters

---
+++ LeetCode.Tests/0001-0050/003_LongestSubstringWithoutRepeatingCharacters_Test.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode._0001_0050;
+
+namespace LeetCode.Tests._0001_0050
+{
+    public class _003_LongestSubstringWithoutRepeatingCharacters_Test
+    {
+        [Test]
+        public void LongestSubstringWithoutRepeatingCharacters_StringIsEmpty_ReturnsZero()
+        {
+            var solution = new _003_Longest_Substring_Without_Repeating_Characters();
+            var result = solution.LengthOfLongestSubstring("");
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void LongestSubstringWithoutRepeatingCharacters_HasWhiteSpace_ReturnsOne()
+        {
+            var solution = new _003_Longest_Substring_Without_Repeating_Characters();
+            var result = solution.LengthOfLongestSubstring(" ");
+            Assert.That(result, Is.EqualTo(1));
+        }
+
+        [Test]
+        [TestCase("abcd", 4)]
+        [TestCase("abcabcbb", 3)]
+        [TestCase("bbbbb", 1)]
+        [TestCase("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz", 26)]
+        [TestCase("abcdefghijklmnopqrstuvwxyzabcdefghijk  lmnopqrstuvwxyz", 27)]
+        public void LongestSubstringWithoutRepeatingCharacters_HasWhiteSpace_ReturnsOne(string str, int expectedLength)
+        {
+            var solution = new _003_Longest_Substring_Without_Repeating_Characters();
+            var result = solution.LengthOfLongestSubstring(str);
+            Assert.That(result, Is.EqualTo(expectedLength));
+        }
+    }
+}
---
+++ LeetCode/0001-0050/003_Longest_Substring_Without_Repeating_Characters.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode._0001_0050
+{
+    public class _003_Longest_Substring_Without_Repeating_Characters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (s == "")
+            {
+                return 0;
+            }
+
+            if (s == " ")
+            {
+                return 1;
+            }
+
+            Dictionary<char, int> lastSeen = new Dictionary<char, int>();
+            int[] longest = { 0, 1 };
+            int startIdx = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char letter = s[i];
+                if (lastSeen.ContainsKey(letter))
+                {
+                    startIdx = Math.Max(startIdx, lastSeen[letter] + 1);
+                }
+
+                if (longest[1] - longest[0] < i + 1 - startIdx)
+                {
+                    longest = new int[] { startIdx, i + 1 };
+                }
+
+                lastSeen[letter] = i;
+            }
+
+            string result = s.Substring(longest[0], longest[1] - longest[0]);
+            return result.Length;
+        }
+    }
+}
