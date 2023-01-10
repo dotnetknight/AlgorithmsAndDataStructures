d90c9f1654c993905cdd0e45680912dc6ca97dd6
Niko Kantaria <niko.kantaria@dataart.com>
2023-01-10 12:53:31 +0400

    7. Reverse Integer

---
+++ LeetCode.Tests/0001-0050/_007_ReverseInteger_Test.cs
@@ -0,0 +1,21 @@
+﻿
+using LeetCode._0001_0050;
+
+namespace LeetCode.Tests._0001_0050
+{
+    public class _007_ReverseInteger_Test
+    {
+        [Test]
+        [TestCase(123, 321)]
+        [TestCase(-1, -1)]
+        [TestCase(0, 0)]
+        [TestCase(1534236469, 0)]
+        public void Reverse_HasString_ReversesInteger(int x, int expectedResult)
+        {
+            var solution = new _007_ReverseInteger();
+            var result = solution.Reverse(x);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ LeetCode/0001-0050/_007_ReverseInteger.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode._0001_0050
+{
+    public class _007_ReverseInteger
+    {
+        public int Reverse(int x)
+        {
+            long reversed = 0;
+            while (x != 0)
+            {
+                reversed = (reversed * 10) + (x % 10);
+                x /= 10;
+
+                if (reversed < int.MinValue || reversed > int.MaxValue)
+                {
+                    return 0;
+                }
+            }
+
+            return (int)reversed;
+        }
+    }
+}
