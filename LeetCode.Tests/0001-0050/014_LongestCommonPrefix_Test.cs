45928ca57680ad9fccf402ec6eb156a665fd3240
Niko Kantaria <niko.kantaria@dataart.com>
2023-01-16 12:38:25 +0400

    14. Longest Common Prefix

---
+++ LeetCode.Tests/0001-0050/014_LongestCommonPrefix_Test.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode._0001_0050;
+
+namespace LeetCode.Tests._0001_0050
+{
+    public class _014_LongestCommonPrefix_Test
+    {
+        [Test]
+        [TestCase(new string[0], "")]
+        [TestCase(new string[1] { "abc" }, "abc")]
+        [TestCase(new string[2] { "aa", "a" }, "a")]
+        [TestCase(new string[2] { "a", "aa" }, "a")]
+        [TestCase(new string[3] { "abcd", "abc", "abd" }, "ab")]
+        [TestCase(new string[3] { "abc", "abd", "bcd" }, "")]
+        public void TwoSum(string[] strs, string expected)
+        {
+            var solution = new _014_LongestCommonPrefix();
+            var result = solution.LongestCommonPrefix(strs);
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ LeetCode/0001-0050/014_LongestCommonPrefix.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode._0001_0050
+{
+    public class _014_LongestCommonPrefix
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0)
+            {
+                return string.Empty;
+            }
+            if (strs.Length == 1)
+            {
+                return strs[0];
+            }
+            string prefix = strs[0];
+            for (int i = 1; i < strs.Length; i++)
+            {
+                while (strs[i].IndexOf(prefix) != 0)
+                {
+                    prefix = prefix.Substring(0, prefix.Length - 1);
+                    if (prefix.Length == 0)
+                    {
+                        return "";
+                    }
+                }
+            }
+            return prefix;
+        }
+    }
+}
