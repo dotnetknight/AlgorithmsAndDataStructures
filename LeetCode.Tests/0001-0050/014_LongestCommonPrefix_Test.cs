21cedd0b6d4323124cec4eb2602c3b86d2cc58a8
Niko Kantaria <niko.kantaria@dataart.com>
2023-01-19 13:52:07 +0400

    26. Remove Duplicates from Sorted Array

--- LeetCode.Tests/0001-0050/014_LongestCommonPrefix_Test.cs
+++ LeetCode.Tests/0001-0050/014_LongestCommonPrefix_Test.cs
@@ -11,7 +11,7 @@ namespace LeetCode.Tests._0001_0050
         [TestCase(new string[2] { "a", "aa" }, "a")]
         [TestCase(new string[3] { "abcd", "abc", "abd" }, "ab")]
         [TestCase(new string[3] { "abc", "abd", "bcd" }, "")]
-        public void TwoSum(string[] strs, string expected)
+        public void LongestCommonPrefix(string[] strs, string expected)
         {
             var solution = new _014_LongestCommonPrefix();
             var result = solution.LongestCommonPrefix(strs);
