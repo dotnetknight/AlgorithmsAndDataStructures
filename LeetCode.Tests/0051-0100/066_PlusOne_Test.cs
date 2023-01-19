dcf3dc77351dda8e8cdfa3a7f82c0a19d1b3c8d4
Niko Kantaria <nika.qantaria98@gmail.com>
2023-01-19 16:50:25 +0400

    66. Plus One

---
+++ LeetCode.Tests/0051-0100/066_PlusOne_Test.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode._0051_0100;
+
+namespace LeetCode.Tests._0051_0100
+{
+    public class _066_PlusOne_Test
+    {
+        [Test]
+        [TestCase(new int[] { 1, 2, 3 }, new int[] { 1, 2, 4 })]
+        [TestCase(new int[] { 4, 3, 2, 1 }, new int[] { 4, 3, 2, 2 })]
+        [TestCase(new int[] { 9 }, new int[] { 1, 0 })]
+        [TestCase(new int[] { 9, 9, 9 }, new int[] { 1, 0, 0, 0 })]
+        [TestCase(new int[] { 0 }, new int[] { 1 })]
+        public void PlusOneTest(int[] input, int[] expectedResult)
+        {
+            var solution = new _066_PlusOne();
+            var result = solution.PlusOne(input);
+            CollectionAssert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ LeetCode/0051-0100/066_PlusOne.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode._0051_0100
+{
+    public class _066_PlusOne
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            if (digits.Length == 1 && digits[0] == 9)
+            {
+                return new int[] { 1, 0 };
+            }
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                if (digits[i] < 9)
+                {
+                    digits[i]++;
+                    return digits;
+                }
+                digits[i] = 0;
+            }
+            int[] newDigits = new int[digits.Length + 1];
+            newDigits[0] = 1;
+            return newDigits;
+        }
+    }
+}
