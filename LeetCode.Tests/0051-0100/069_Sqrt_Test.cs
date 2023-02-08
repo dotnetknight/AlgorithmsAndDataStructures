74bedfc5ff6ce9e028d0db8f8d5d7fa1146a73e4
Niko Kantaria <nika.qantaria98@gmail.com>
2023-02-08 17:22:17 +0400

    69. Sqrt(x)

---
+++ LeetCode.Tests/0051-0100/069_Sqrt_Test.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode._0051_0100;
+
+namespace LeetCode.Tests._0051_0100
+{
+    public class _069_Sqrt_Test
+    {
+        [Test]
+        [TestCase(-1, -1)]
+        [TestCase(1, 1)]
+        [TestCase(0, 0)]
+        [TestCase(int.MaxValue, 46340)]
+        [TestCase(4, 2)]
+        public void MySqrt(int x, int expectedResult)
+        {
+            var solution = new _069_Sqrt();
+            var result = solution.MySqrt(x);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ LeetCode/0051-0100/069_Sqrt.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode._0051_0100
+{
+    public class _069_Sqrt
+    {
+        public int MySqrt(int x)
+        {
+            if (x <= 1)
+            {
+                return x;
+            }
+
+            int left = 1, right = x;
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+                if (mid <= x / mid && (mid + 1) > x / (mid + 1)) return mid;
+                else if (mid > x / mid) right = mid;
+                else left = mid + 1;
+            }
+            return left;
+        }
+    }
+}
